38f124274fcb89f862937f280dffff236f6d7bb7
Ari Sherman <arisherm@umich.edu>
2023-03-08 21:58:14 -0500

    Fixed stuff

---
+++ scripts/survey.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.submit').addEventListener('click', function() {
+      // Redirect to the new page
+      window.location.href = 'index.html';
+    });
+  });
