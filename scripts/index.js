94e77197a19564e3ec71a2d7a3c2b9f1dd9ba35b
jshefman <jshefman@umich.edu>
2023-03-08 21:08:11 -0500

    cookie shit

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    if (getCookie('first') != "") {
+        buttons = $(".buttons")
+        buttons[0].style.display = "None"
+        userBox = $(".userBox")
+        userBox[0].style.display = "block"
+        user = $('.user')
+        user[0].innerHTML = user[0].innerHTML + " " + getCookie('first')
+        user = $(".user")
+
+        $('.signout').on('click', function(event) {
+            clearCookie('first');
+        });
+    }
+});
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+  function clearCookie(name) {
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    buttons = $(".buttons")
+    buttons[0].style.display = "block"
+    userBox = $(".userBox")
+    userBox[0].style.display = "none"
+  }
+  
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
     $('#signup').on('submit', function(event) {
       event.preventDefault(); // prevent the default form submission behavior
-      var formData = $(this).serialize(); // get the form data as a string
-      window.location.href = 'restaurant.html';
-      
+      window.location.href = 'survey.html';
+      first = $('#first')[0].value
+      setCookie('first', first, 0.5)
     });
   });
   
+
+  function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
